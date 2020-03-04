0acf27bdc5be919200bae033639cdcc4bad535e8
Mustafa TUNCER <tuncerm@gmail.com>
2020-03-04 23:52:53 +0100

    Finalized the frontend part without the backend connection.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,73 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
+import {BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 
-function App() {
-  return <h1>Let's start!</h1>;
+import Users from './user/pages/Users';
+import UserPlaces from './places/pages/UserPlaces';
+import NewPlace from './places/pages/NewPlace';
+import UpdatePlace from './places/pages/UpdatePlace';
+import Auth from './user/pages/Auth';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+import { AuthContext } from './shared/context/auth-context';
+
+
+const App = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+
+  if(isLoggedIn){
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:userId/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+    );
+  }
+
+  return (
+    <AuthContext.Provider value={{isLoggedIn, login, logout}}>
+      <Router>
+        <MainNavigation />
+        <main>
+          {routes}
+        </main>
+      </Router>
+    </AuthContext.Provider>
+  );
 }
 
 export default App;
